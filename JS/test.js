0812281623f82d78dd7edbf867d86041d65471da
sanjay-96 <122206843+sanjay-96@users.noreply.github.com>
2023-12-18 23:57:47 +1100

    Update test.js

--- JS/test.js
+++ JS/test.js
@@ -1,1 +1,9 @@
-console.log('hi sanjay')
+const accountId = 123456
+let accountEmail="sanjay2@gmail.com"
+var accountPassword = "546454"
+accountCity = "japan"
+
+
+accountId=2
+
+console.log(accountId);
